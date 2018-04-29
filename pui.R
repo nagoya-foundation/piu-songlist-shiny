ac861b6dd23fed67da7a7db72abd58f275e74ad7
Filipe Filardi <filipe.jesus@usp.br>
2018-04-29 16:27:47 -0300

    Add first kind of working version

--- pui.R
+++ pui.R
@@ -1,8 +1,25 @@
-library(dplyr)
 library(shiny)
 
+# Read CSV
 r <- read.csv("Desktop/piu-songlist-shiny/pumpList.csv", stringsAsFactors = F)
 
+# Start conversion of levels
+speed <- r$LEVEL
+double <- r$LEVEL
+coop <- r$LEVEL
+sperformace <- r$LEVEL
+dperformace <- r$LEVEL
+
+
+speed <- gsub("S", "", gsub("\\b\\s*([^S]\\d+|[A-z]{2,}\\d+)\\b", "", speed))
+double <- gsub("D", "", gsub("\\b\\s*([^D]\\d+|[A-z]{2,}\\d+)\\b", "", double))
+#coop <- gsub("CO", "", gsub("\\b\\s*([^CO]\\d+|[A-z]{2,}\\d+)\\b", "", coop))
+#sperformace <- gsub("SP", "", gsub("\\b\\s*([^SP]\\d+|[A-z]{2,}\\d+)\\b", "", sperformace))
+#dperformace <- gsub("DP", "", gsub("\\b\\s*([^DP]\\d+|[A-z]{2,}\\d+)\\b", "", dperformace))
+
+r$LEVEL <- speed
+
+# User Interface
 ui <- fluidPage(
     
     titlePanel("Pump It Up Songlist"),
@@ -15,7 +32,7 @@ ui <- fluidPage(
                selectInput("level", "Level", append("All", seq(1:28)))
         ),
         column(4,
-               selectInput("category", "Category", c("All", "Speed", "Double", "Co-op"))
+               selectInput("category", "Category", c("Speed", "Double", "Co-op", "Single Performace", "Double Performace"))
         )
     ),
     
@@ -26,24 +43,38 @@ ui <- fluidPage(
     )
 )
 
-# Define server logic to plot various variables against mpg ----
+# Server
 server <- function(input, output) {
-    
-    # Filter data based on selections
     output$table <- DT::renderDataTable(DT::datatable({
-        # data <- mpg
-        # if (input$man != "All") {
-        #     data <- data[data$manufacturer == input$man,]
-        # }
-        # if (input$cyl != "All") {
-        #     data <- data[data$cyl == input$cyl,]
-        # }
-        # if (input$trans != "All") {
-        #     data <- data[data$trans == input$trans,]
-        # }
-        r
+        if (input$game != "All") {
+            r <- r[r$GAME == input$game,]
+        }
+        if (input$category != "Speed") {
+            if(input$category == "Double"){
+            	
+            }
+        	
+        	switch (input$category,
+        		"Double" = {
+        			r$LEVEL <- double
+        		},
+        		"Co-op" = {
+        			r$LEVEL <- coop
+        		},
+        		"Single Performace" = {
+        			r$LEVEL <- dperformace
+        		},
+        		{
+        			r$LEVEL <- sperformace
+        		}
+			)
+        }
+        if (input$level != "All") {
+        	r <- r[lapply(strsplit(r$LEVEL, " "),function(x) length(grep(paste0("\\b^", input$level, "\\b"), x))) > 0, ]
+        }
+		r
     }))
 }
 
+# Create Shiny app object
 shinyApp(ui, server)
-
